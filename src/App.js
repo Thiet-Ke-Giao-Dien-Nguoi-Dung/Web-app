f36e3917569876278f8b0789b701202b11f26146
phungMai234 <phungtuyet234@gmail.com>
2019-10-24 23:23:18 +0700

    build mockup v1

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 
 import Home from "./components/home/home"
-import Login from "./components/login/login";
-import Register from "./components/register/register";
 
 
 import {Route,Switch} from "react-router-dom";
@@ -12,9 +10,7 @@ class App extends React.Component{
   render() {
       return (
           <Switch>
-              <Route path ="/home" component={Home}/>
-              <Route path="/login" exact component={Login}/>
-              <Route path="/register" component={Register}/>
+              <Route path ="/" component={Home}/>
           </Switch>
 
     )
---
+++ src/components/modal/modal.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "./style.css"
+
+class Modal extends React.Component {
+    render() {
+        if(!this.props.show) {
+            return null;
+        }
+
+        return (
+            <div className="backdrop">
+                <div className="modal">
+
+
+                    <div className="modal-header">
+                        <h2 className="modal-title">{this.props.title}</h2>
+                        <button className="btn-close" onClick={this.props.onClose}>
+                            x
+                        </button>
+                    </div>
+
+
+                    <div className="modal-content">
+                        {this.props.childrenContent}
+                    </div>
+
+
+                    <div className="modal-help">
+                        {this.props.childrenHelp}
+                    </div>
+
+                    <div className="modal-footer">
+                        {this.props.childrenFooter}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+Modal.propTypes = {
+    title: PropTypes.string,
+    onClose: PropTypes.func.isRequired,
+    show: PropTypes.bool,
+    childrenContent: PropTypes.node,
+    childrenHelp: PropTypes.node,
+    childrenFooter: PropTypes.node
+};
+
+export default Modal;
